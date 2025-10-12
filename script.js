5f188889f1b3a264daac65926f179be32b28233c
milty90 <156122688+milty90@users.noreply.github.com>
2025-10-12 20:10:22 +0200

    bugs fixing before deploying

--- script.js
+++ script.js
@@ -6,45 +6,28 @@ function appendToDisplay(value) {
 
   if (value === "=") {
     try {
-      const calculation = display.value; // Az eredeti számítás
-      const result = eval(display.value); // Az eredmény
+      const calculation = display.value;
+      const result = eval(display.value);
 
-      // Szóközök hozzáadása a műveleti jelekhez a megjelenítéshez
       const formattedCalculation = calculation
         .replace(/\+/g, " + ")
         .replace(/\-/g, " - ")
         .replace(/\*/g, " * ")
         .replace(/\//g, " / ");
 
-      const fullCalculation = `${formattedCalculation} = ${result}`; // Teljes számítás formátum szóközökkel
+      const fullCalculation = `${formattedCalculation} = ${result}`;
 
-      // Hozzáadás a listához
       resultsList.push(fullCalculation);
 
-      // // Eltávolítjuk az animációt az előző elemről (ha van)
-      // const previousFirst = resultsListElement.querySelector("li:first-child");
-      // if (previousFirst) {
-      //   previousFirst.style.animation = "none";
-      //   previousFirst.style.width = "26ch";
-      //   previousFirst.style.borderRight = "none";
-      // }
-
-      // Új lista elem létrehozása és hozzáadása a HTML-hez
       const listItem = document.createElement("li");
       listItem.textContent = fullCalculation;
       resultsListElement.appendChild(listItem);
 
-      // // Kurzor eltávolítása az animáció végén
-      // setTimeout(() => {
-      //   listItem.style.borderRight = "none";
-      // }, 3500); // 3s typing + 0.5s extra idő
-
-      // Az eredmény megjelenítése a display-ben
       display.value = result;
 
-      console.log("Calculation added:", fullCalculation);
+      // console.log("Calculation added:", fullCalculation);
     } catch (error) {
-      display.value = "Error";
+      display.value = "Fehler";
     }
   } else {
     display.value += value;
